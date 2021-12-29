d3e490e6a63e15b0b0cdb965b50f3673de498158
pruth <pruth@LAPTOP-I8AG8EMN>
2021-12-29 07:21:15 +0530

    Commit all

---
+++ Proj4/src/test/java/pack1/NewTest.java
@@ -0,0 +1,21 @@
+package pack1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+  @Test
+  public void proj4() {
+	  
+	  System.out.println("Proj4");
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
